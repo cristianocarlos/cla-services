53f68ba3c59bf2e242de6b05b274b02431797ba4
Cristiano Carlos <cristianoc@gmail.com>
2026-07-03 03:12:31 -0300

    .

--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\DumbController;
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\GeoController;
 use App\Models\ApiToken;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api.token'])->group(function () {
     Route::get('/authentication/token', [AuthenticationController::class, 'token']);
+    //
+    Route::post('/file/presign', [FileController::class, 'apiCloudinaryPresign']);
     //
     Route::get('/geo/address', [GeoController::class, 'address']);
     Route::get('/geo/city', [GeoController::class, 'city']);
